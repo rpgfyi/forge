49566dd30776adae1db4b8e087a997964659f087
Matt Buresh <matt@mburesh.com>
2018-07-06 00:37:44 -0500

    Update footer links. Update "Durable" tag.

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,32 @@
 <template>
     <footer class="footer">
         <div class="content has-text-centered">
-            <p>An <a href="https://rpg.fyi">rpg.fyi</a> project, built by <a href="https://www.twitter.com/matt_buresh">Matt Buresh</a></p>
+            <p>An <a href="https://rpg.fyi">rpg.fyi</a> project, made by <a href="https://www.twitter.com/matt_buresh">Matt Buresh</a></p>
+            
+            <b-field class="has-addons-centered">
+
+                <div class="control">
+                    <a class="button" href="https://github.com/rpgfyi/forge">
+                        <b-icon
+                            icon="github-circle"
+                            size="is-small">
+                        </b-icon>
+                        <span>Github</span>
+                    </a>
+                </div>
+
+                <div class="control">
+                    <a class="button" href="https://www.twitter.com/rpgfyi">
+                        <b-icon
+                            icon="twitter"
+                            size="is-small">
+                        </b-icon>
+                        <span>Twitter</span>
+                    </a>
+                </div>
+
+            </b-field>
+
         </div>
     </footer>
 </template>
